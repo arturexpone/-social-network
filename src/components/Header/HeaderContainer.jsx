32af650281ea7ae2fd8037cff661afa6405e9c20
arturexpone <arturexpone@yandex.ru>
2020-04-26 21:08:54 +0300

    added information about the login

---
+++ src/components/Header/HeaderContainer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {connect} from "react-redux";
+import * as axios from "axios";
+import {setAuthUserData} from "../../redux/auth-reducer";
+import Header from "./Header";
+
+class HeaderContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true})
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    this.props.setAuthUserData(response.data.data);
+                }
+            })
+    }
+
+    render() {
+        return <Header {...this.props}/>
+    }
+}
+
+let mapStateToProps = (state) => ({
+    login: state.auth.login,
+    isAuth: state.auth.isAuth
+});
+
+export default connect(mapStateToProps, {setAuthUserData})(HeaderContainer);
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
+import {authReducer} from "./auth-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
-    users: usersReducer
+    users: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore(reducers, +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
