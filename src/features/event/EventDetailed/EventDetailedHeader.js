c327005c68116525f844c5c530f044ccfe295264
arturexpone <arturexpone@yandex.ru>
2020-06-20 15:19:49 +0300

    adding the event detailed page header component

--- src/features/event/EventDetailed/EventDetailedHeader.js
+++ src/features/event/EventDetailed/EventDetailedHeader.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import {Button, Header, Image, Item, Segment} from 'semantic-ui-react';
+
+import drinks from '../../../assets/categoryImages/drinks.jpg'
 
 export const EventDetailedHeader = (props) => {
+
+  const eventImageStyle = {
+    filter: 'brightness(30%)'
+  };
+
+  const eventImageTextStyle = {
+    position: 'absolute',
+    bottom: '5%',
+    left: '5%',
+    width: '100%',
+    height: 'auto',
+    color: 'white'
+  };
+
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Segment.Group>
+      <Segment basic attached="top" style={{ padding: '0' }}>
+        <Image src={drinks} fluid style={eventImageStyle}/>
+
+        <Segment basic style={eventImageTextStyle}>
+          <Item.Group>
+            <Item>
+              <Item.Content>
+                <Header
+                  size="huge"
+                  content="Event Title"
+                  style={{ color: 'white' }}
+                />
+                <p>Event Date</p>
+                <p>
+                  Hosted by <strong>Hosted by</strong>
+                </p>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Segment>
+      </Segment>
+
+      <Segment attached="bottom">
+        <Button>Cancel My Place</Button>
+        <Button color="teal">Join this event</Button>
+
+        <Button color="orange" floated="right">
+          Manage Event
+        </Button>
+      </Segment>
+    </Segment.Group>
   );
 }
