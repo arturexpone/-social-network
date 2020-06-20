5246c21e3205bb7d0d4676eb43e9fdf0c8205ee1
arturexpone <arturexpone@yandex.ru>
2020-06-20 16:14:02 +0300

    add detailed sidebar

--- src/features/event/EventDetailed/EventDetailedSidebar.js
+++ src/features/event/EventDetailed/EventDetailedSidebar.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {Item, Label, List, Segment} from 'semantic-ui-react';
+
+import user from '../../../assets/images/user.png'
 
 export const EventDetailedSidebar = (props) => {
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+    <>
+      <Segment
+        textAlign='center'
+        style={{ border: 'none' }}
+        attached='top'
+        secondary
+        inverted
+        color='teal'
+      >
+        2 People Going
+      </Segment>
+      <Segment attached>
+        <List relaxed divided>
+          <Item style={{ position: 'relative' }}>
+            <Label
+              style={{ position: 'absolute' }}
+              color='orange'
+              ribbon='right'
+            >
+              Host
+            </Label>
+            <Item.Image size='tiny' src={user} />
+            <Item.Content verticalAlign='middle'>
+              <Item.Header as='h3'>
+                <a>Attendee Name</a>
+              </Item.Header>
+            </Item.Content>
+          </Item>
+        </List>
+      </Segment>
+    </>
   );
 }
