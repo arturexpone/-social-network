8e723dc7ff8b8c23f72332433dfab4ba39b07a17
arturexpone <arturexpone@yandex.ru>
2020-06-22 13:58:22 +0300

    creating a reusable date input field

---
+++ src/features/common/form/DateInput.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import DatePicker from 'react-datepicker/es';
+import {Form, Label} from 'semantic-ui-react';
+import 'react-datepicker/dist/react-datepicker.css';
+
+export const DateInput = (props) => {
+
+  const {
+    input, width,
+    placeholder,
+    meta: {touched, error},
+    ...rest
+  } = props;
+
+  return (
+    <Form.Field error={touched && !!error}>
+      <DatePicker
+        {...rest}
+        placeholder={placeholder}
+        selected={input.value ? new Date(input.value) : null}
+        onChange={input.onChange}
+      />
+      {touched && error && <Label basic color='red'>{error}</Label>}
+    </Form.Field>
+  );
+}
