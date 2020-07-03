60bf64d5adbdf93163b8d7fe0513cd0c9b9ad980
Artur <arturexpone@yandex.ru>
2020-07-03 15:27:11 +0300

    clean master

--- src/utils/validators/validators.js
+++
@@ -1,8 +0,0 @@
-export const required = (value) => {
-    if (value) return undefined;
-    return 'Please enter your details';
-}
-
-export const maxLengthCreator = (maxLength) => (value) => {
-    return value && value.length > maxLength ? `Max length is ${maxLength} symbols` : undefined;
-}
