20c07d529d4c4800275ac05d097f6e3aa10cfbe9
Artur <arturexpone@yandex.ru>
2020-07-09 20:25:07 +0300

    unclose build

---
+++ build/precache-manifest.75933b162029a30ce33969794fa80590.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9921fa85232ec96c21a1b302d26f8d7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "682c35d535f286e1d331",
+    "url": "/static/css/2.d03e0a67.chunk.css"
+  },
+  {
+    "revision": "6360e0c3bb341902010c",
+    "url": "/static/css/main.eed35357.chunk.css"
+  },
+  {
+    "revision": "682c35d535f286e1d331",
+    "url": "/static/js/2.b6128724.chunk.js"
+  },
+  {
+    "revision": "cb4af6d8e360806c3dd1d69fcd8ca177",
+    "url": "/static/js/2.b6128724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6360e0c3bb341902010c",
+    "url": "/static/js/main.4202eddb.chunk.js"
+  },
+  {
+    "revision": "54df64a3d30a340b1b63",
+    "url": "/static/js/runtime-main.66257bb2.js"
+  },
+  {
+    "revision": "b98b66575f01bdc8d6cb80a9888330ce",
+    "url": "/static/media/culture.b98b6657.jpg"
+  },
+  {
+    "revision": "71c2a561b5fb7a5840f9a83e93e834ee",
+    "url": "/static/media/drinks.71c2a561.jpg"
+  },
+  {
+    "revision": "0e8ebe96be13129156c121853c12d6fd",
+    "url": "/static/media/film.0e8ebe96.jpg"
+  },
+  {
+    "revision": "1f3599ab361be629cf258ae3706f88d5",
+    "url": "/static/media/food.1f3599ab.jpg"
+  },
+  {
+    "revision": "ca543039ef3701060be485181ab3a437",
+    "url": "/static/media/music.ca543039.jpg"
+  },
+  {
+    "revision": "27ad1ef6aecd0009bc9318caf306d907",
+    "url": "/static/media/travel.27ad1ef6.jpg"
+  }
+]);
