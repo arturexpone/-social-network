997263b3e49656ebed3bd28d828a53302e6f9f14
Artur <arturexpone@yandex.ru>
2020-07-10 19:50:17 +0300

    attaching auth is rady to the app

--- src/features/user/Settings/AccountPage.js
+++ src/features/user/Settings/AccountPage.js
@@ -8,7 +8,6 @@ import {compose} from 'redux';
 
 import {TextInput} from '../../common/form/TextInput';
 import {updatePassword} from '../../../store/ac';
-import handleSubmit from 'redux-form/lib/handleSubmit';
 
 const validate = combineValidators({
   newPassword1: isRequired({message: 'Please enter a password'}),
@@ -18,68 +17,85 @@ const validate = combineValidators({
   )()
 });
 
-const AccountPage = ({ error, updatePassword, handleSubmit }) => {
+const AccountPage = ({ error, updatePassword, handleSubmit, provider }) => {
+
+  const noProvider = (
+        <div>
+          <Header color='teal' sub content='Change password' />
+          <p>Use this form to update your account settings</p>
+          <Form onSubmit={handleSubmit(updatePassword)}>
+            <Field
+              width={8}
+              name='newPassword1'
+              type='password'
+              pointing='left'
+              inline={true}
+              component={TextInput}
+              basic={true}
+              placeholder='New Password'
+            />
+            <Field
+              width={8}
+              name='newPassword2'
+              type='password'
+              inline={true}
+              basic={true}
+              pointing='left'
+              component={TextInput}
+              placeholder='Confirm Password'
+            />
+            {error && (
+              <Label basic color='red'>
+                {error}
+              </Label>
+            )}
+            <Divider />
+            <Button size='large' positive content='Update Password' style={{marginBottom: '15px'}}/>
+          </Form>
+        </div>
+  );
+
+  const googleProvider = (
+    <div>
+      <Header color='teal' sub content='Google Account' />
+      <p>Please visit Google to update your account settings</p>
+      <Button type='button' color='google plus'>
+        <Icon name='google plus' />
+        Go to Google
+      </Button>
+    </div>
+  );
+
+  const fbProvider = (
+    <div>
+      <Header color='teal' sub content='Facebook Account' />
+      <p>Please visit Facebook to update your account settings</p>
+      <Button type='button' color='facebook'>
+        <Icon name='facebook' />
+        Go to Facebook
+      </Button>
+    </div>
+  );
 
   return (
     <Segment>
       <Header dividing size='large' content='Account' />
-      <div>
-        <Header color='teal' sub content='Change password' />
-        <p>Use this form to update your account settings</p>
-        <Form onSubmit={handleSubmit(updatePassword)}>
-          <Field
-            width={8}
-            name='newPassword1'
-            type='password'
-            pointing='left'
-            inline={true}
-            component={TextInput}
-            basic={true}
-            placeholder='New Password'
-          />
-          <Field
-            width={8}
-            name='newPassword2'
-            type='password'
-            inline={true}
-            basic={true}
-            pointing='left'
-            component={TextInput}
-            placeholder='Confirm Password'
-          />
-          {error && (
-            <Label basic color='red'>
-              {error}
-            </Label>
-          )}
-          <Divider />
-          <Button size='large' positive content='Update Password' style={{marginBottom: '15px'}}/>
-        </Form>
-      </div>
+      {provider === 'password' && !provider
+          ? noProvider
+          : provider === 'google.com'
+            ? googleProvider
+            : provider === 'facebook.com'
+              ? fbProvider : null
+      }
 
-      <div>
-        <Header color='teal' sub content='Facebook Account' />
-        <p>Please visit Facebook to update your account settings</p>
-        <Button type='button' color='facebook'>
-          <Icon name='facebook' />
-          Go to Facebook
-        </Button>
-      </div>
-
-      <div>
-        <Header color='teal' sub content='Google Account' />
-        <p>Please visit Google to update your account settings</p>
-        <Button type='button' color='google plus'>
-          <Icon name='google plus' />
-          Go to Google
-        </Button>
-      </div>
     </Segment>
   );
 };
 
 export default compose(
   reduxForm({ form: 'account', validate }),
-  connect(null, {updatePassword})
+  connect((state) => ({
+    provider: state.firebase.auth.providerData[0].providerId
+  }), {updatePassword})
 )
 (AccountPage);
